0b6872d557cbdaab9d4bbec989db63b2ffdfbf31
Swapnil Jakhi <80448432+swapnil99jakhi@users.noreply.github.com>
2021-04-27 12:32:00 +0530

    Update main.c

--- Activity1/src/main.c
+++ Activity1/src/main.c
@@ -1,1 +1,23 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include"../inc/pin_config.h"
+int main(void)
+{
+
+    pin_config();
+    while(1)
+    {
+        if(!(PIND&(1<<PD2)) && !(PIND&(1<<PD3)))
+        {
+        PORTB|=(1<<PB0);
+        _delay_ms(200);
+        }
+        else
+        {
+            PORTB&=~(1<<PB0);
+            _delay_ms(200);
+        }
+    }
+    return 0;
+}
 
