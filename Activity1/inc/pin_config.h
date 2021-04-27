57afe93080c4b1866be2516332d4b8b4638509c7
Swapnil Jakhi <80448432+swapnil99jakhi@users.noreply.github.com>
2021-04-27 12:29:37 +0530

    Add files via upload

---
+++ Activity1/inc/pin_config.h
@@ -0,0 +1,19 @@
+/**
+ * @file pin_config.h
+ * @author Swapnil Jakhi
+ * @brief Header file to define pin configuration 
+ * @version 0.1
+ * @date 2021-04-27
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef PIN_CONFIG_H_INCLUDED
+#define PIN_CONFIG_H_INCLUDED
+/**
+ * @brief Will define configuration of different pins
+ */
+void pin_config();
+
+
+#endif // PIN_CONFIG_H_INCLUDED
