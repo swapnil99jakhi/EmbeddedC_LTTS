657899c6e0eda463d33bc03743f281722cf19fe8
Swapnil Jakhi <80448432+swapnil99jakhi@users.noreply.github.com>
2021-04-27 12:32:32 +0530

    Add files via upload

---
+++ Activity1/src/pin_config.c
@@ -0,0 +1,14 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include"pin_config.h"
+void pin_config()
+{
+   // Direction of ports
+    DDRB|=(1<<PB0);
+    DDRD&=~(1<<PD2);
+    DDRD&=~(1<<PD3);
+
+    // PULL-UP +5V for push-buttons
+    PORTD|=(1<<PD2);
+    PORTD |=(1<<PD3);
+}
