6de46358991cfa3bb5663eceecd466f9379dc204
Swapnil Jakhi <80448432+swapnil99jakhi@users.noreply.github.com>
2021-04-28 12:36:58 +0530

    Add files via upload

---
+++ src/activity1.c
@@ -0,0 +1,27 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include"activity1.h"
+    // Direction of ports
+void activity1(){
+    DDRB|=(1<<PB0);
+    DDRD&=~(1<<PD2);
+    DDRD&=~(1<<PD3);
+
+    // PULL-UP +5V for push-buttons
+    PORTD|=(1<<PD2);
+    PORTD |=(1<<PD3);
+    while(1)
+    {   // Acts as an AND gate for the two switches
+        if(!(PIND&(1<<PD2)) && !(PIND&(1<<PD3)))
+        {
+        // port B at logic 1
+        PORTB|=(1<<PB0);
+        _delay_ms(200);
+        }
+        else
+        {   // port B at logic 0
+            PORTB&=~(1<<PB0);
+            _delay_ms(200);
+        }
+    }
+}
