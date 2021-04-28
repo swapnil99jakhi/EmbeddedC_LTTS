e6aa3c86ebe162f89beadadada03fead18743d9e
Swapnil Jakhi <80448432+swapnil99jakhi@users.noreply.github.com>
2021-04-28 12:36:13 +0530

    Add files via upload

---
+++ inc/activity1.h
@@ -0,0 +1,17 @@
+/**
+ * @file activity1.h
+ * @author Swapnil Jakhi
+ * @brief Header file to define pin configuration
+ * @version 0.1
+ * @date 2021-04-27
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+#ifndef ACTIVITY1_H_INCLUDED
+#define ACTIVITY1_H_INCLUDED
+/**
+ * @brief LED turns ON(passenger is seated) when both the button switch and heater switch are closed
+ */
+void activity1();
+#endif // ACTIVITY1_H_INCLUDED
