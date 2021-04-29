a8dc8fbca94d7e0fa52e72db7fdfd375ca246fe9
Swapnil Jakhi <80448432+swapnil99jakhi@users.noreply.github.com>
2021-04-29 23:09:16 +0530

    Update activity4.h

--- inc/activity4.h
+++ inc/activity4.h
@@ -3,5 +3,5 @@
 void USARTInit(uint16_t ubrr_value);
 char USARTReadChar();
 void USARTWriteChar( uint16_t data);
-void activity4(uint16_t temperature);
+void activity4(uint16_t temp);
 #endif // ACTIVITY4_H_INCLUDED
