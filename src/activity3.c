f9aae162c21c6971cfe48a28125051f4bc5b1627
Swapnil Jakhi <80448432+swapnil99jakhi@users.noreply.github.com>
2021-04-29 22:55:37 +0530

    Add files via upload

---
+++ src/activity3.c
@@ -0,0 +1,40 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include "activity3.h"
+#include "activity2.h"
+#include "activity4.h"
+unsigned volatile temperature;
+void Timer()
+{
+    TCCR1A|=(1<<COM1A1)|(1<<WGM11)|(1<<WGM10);
+    TCCR1B|=(1<<WGM12)|(1<<CS11)|(1<<CS10); //64 Prescalar
+    DDRB|=(1<<PB1);
+}
+int activity3()
+{
+
+    Timer();
+    OCR1A=ADCregister;
+    if(ADCregister>0 && ADCregister<=200)
+    {
+        temperature=20;
+        activity4(temperature);
+    }
+    else if(ADCregister>200 && ADCregister<=500)
+    {
+        temperature=25;
+        activity4(temperature);
+    }
+    else if(ADCregister>500 && ADCregister<710)
+    {
+        temperature=29;
+        activity4(temperature);
+    }
+    else if(ADCregister>710 && ADCregister<1024)
+    {
+        temperature=33;
+        activity4(temperature);
+    }
+    _delay_ms(2000);
+    return 0;
+}
