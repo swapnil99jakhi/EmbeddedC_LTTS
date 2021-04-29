8314df889e97a3e93fc8ca0bf0b6863a570e0cce
Swapnil Jakhi <80448432+swapnil99jakhi@users.noreply.github.com>
2021-04-29 23:03:55 +0530

    Update SeatHeatingApp.c

--- SeatHeatingApp.c
+++ SeatHeatingApp.c
@@ -1,9 +1,15 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include"inc/activity1.h"
+#include"inc/activity2.h"
+#include"inc/activity3.h"
+#include"inc/activity4.h"
 int main(void)
 {
 
     activity1();
+    activity2();
+    activity3();
+    activity4();
     return 0;
 }
