2948d0cefc0ba13b4d52d0b991942b0cd4935172
Swapnil Jakhi <80448432+swapnil99jakhi@users.noreply.github.com>
2021-04-30 11:56:02 +0530

    Update activity3.h

--- inc/activity3.h
+++ inc/activity3.h
@@ -1,6 +1,12 @@
 #ifndef ACTIVITY3_H_INCLUDED
 #define ACTIVITY3_H_INCLUDED
+/**
+* @brief Function which initializes Timer 1 in form of register A and B and also gives direction for oscilloscope port
+*/
 void Timer();
+/**
+* @brief Function which compare the ADCRegister values with the threshold and sets the ideal temperature to be transmitted to the serial monitor
+*/
 int activity3();
 #endif // ACTIVITY3_H_INCLUDED
 
