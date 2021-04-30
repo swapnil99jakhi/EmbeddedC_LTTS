e47d0099e2e891652ca32a33409934f04ca8bfdc
Swapnil Jakhi <80448432+swapnil99jakhi@users.noreply.github.com>
2021-04-30 12:02:23 +0530

    Update activity4.h

--- inc/activity4.h
+++ inc/activity4.h
@@ -1,7 +1,19 @@
 #ifndef ACTIVITY4_H_INCLUDED
 #define ACTIVITY4_H_INCLUDED
+/**
+* @brief This function sets the baud rate with which the bits are to be transmitted to the serial monitor
+*/
 void USARTInit(uint16_t ubrr_value);
+/**
+* @brief This function reads the character value from the serial monitor and display it
+*/
 char USARTReadChar();
+/**
+* @brief This function writes the data into the serial monitor/ transmit the data value to the serial port and display it
+*/
 void USARTWriteChar( uint16_t data);
+/**
+* @brief This function takes input as an ideal temperature and call function USARTWriteChar to write that temperature into serial monitor and display it
+*/
 void activity4(uint16_t temp);
 #endif // ACTIVITY4_H_INCLUDED
